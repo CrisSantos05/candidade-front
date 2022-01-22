4cf2859bf1b395a0beb4c391b51410d119e5bfb3
CrisSantos05 <cristianosantos0584@gmail.com>
2022-01-21 21:29:28 -0300

    fix: fixing proxy config

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,8 +1,9 @@
 const proxy = [
   {
-    context: '/api',
-    target: 'http://localhost:8080',
-    pathRewrite: {'^/api' : ''}
+    context: ['/api'],
+    target: 'http://localhost:8080/',
+    secure: "false",
+    logLevel: "debug"
   }
 ];
 module.exports = proxy;
